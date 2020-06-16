bd56183a0d47f23c838ba7575fdd12c4b4986d21
Kyle Yates <53956594+KyleTy1er@users.noreply.github.com>
2020-06-16 16:58:56 -0700

    finished assignment

--- src/sorting/sorting.py
+++ src/sorting/sorting.py
@@ -1,28 +1,75 @@
+
 # TO-DO: complete the helper function below to merge 2 sorted arrays
+
+# how to merge two sorted arrays?
+# how to tell which array goes first?
+        # The last element should tell the order?
+        # should always be able to compare with list[-1]
+
+
 def merge(arrA, arrB):
+    # stores the length of both arrays in an int value:
     elements = len(arrA) + len(arrB)
+    # creates an array of zeros the exact length of our combined arrays.
     merged_arr = [0] * elements
 
-    # Your code here
+    # main iterator
+    m_i = 0
+    # left iterator
+    l_i = 0
+    # right iterator
+    r_i = 0
 
+    # while left iterator is less than length of left array AND
+    # while right iterator is less than length of right array
+    while (l_i < len(arrA)) and (r_i < len(arrB)):
+
+        # if right array value at this iteration is less than left array value
+        # update the merged array of this iteration location with main iterator value
+        # and update the left iterator value for the next pass
+        if arrA[l_i] <= arrB[r_i]:
+            merged_arr[m_i] = arrA[l_i]
+            l_i += 1
+
+        # if not less than or equal to, do the reverse and store the right iterator value
+        # in the iterator value of m_i
+        else:
+            merged_arr[m_i] = arrB[r_i]
+            r_i += 1
+
+        # after each of the above passes update the main iterator value:
+        m_i += 1
+
+    # while left iterator is less than length of left array but right iterator is not:
+    # meaning the right array has been appended and the left has remaining values:
+    # store the sorted values each pass into merged_arr
+    while (l_i < len(arrA)):
+        merged_arr[m_i] = arrA[l_i]
+        l_i += 1
+        m_i += 1
+
+    # reverse of the above to account for expenditure of left array:
+    while (r_i < len(arrB)):
+        merged_arr[m_i] = arrB[r_i]
+        r_i += 1
+        m_i += 1
 
     return merged_arr
 
-# TO-DO: implement the Merge Sort function below recursively
-def merge_sort(arr):
-    # Your code here
+# # TO-DO: implement the Merge Sort function below recursively
+# what is the base case/terminating case?
 
+def merge_sort(arr):
+    # base case is if the length of the array is less than 1
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        left = merge_sort(left)
+        right = merge_sort(right)
+
+        arr = merge(left, right)
 
     return arr
-
-# STRETCH: implement the recursive logic for merge sort in a way that doesn't 
-# utilize any extra memory
-# In other words, your implementation should not allocate any additional lists 
-# or data structures; it can only re-use the memory it was given as input
-def merge_in_place(arr, start, mid, end):
-    # Your code here
-
-
-def merge_sort_in_place(arr, l, r):
-    # Your code here
 
